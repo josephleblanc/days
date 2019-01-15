ae096e25c1f91c54373dcf23c56bc20c4ba6a326
josephleblanc <joseph.t.leblanc@protonmail.com>
2019-01-15 12:21:02 -0900

    Added ex16 and did some preliminary testing to match with strcat from <string.h>

---
+++ d05/ex16/ft_strcat.c
@@ -0,0 +1,18 @@
+// Don't forget to add a header!
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j])
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	return (dest);
+}
